d0f36d41cbff1b2e266caf90ef05a5168bea0e17
Nicolas Szkwir <nicoszkwir@gmail.com>
2024-02-21 15:14:23 +0100

    Added DI NetworkModule

---
+++ network/src/main/java/com/spitzer/network/di/NetworkModule.kt
@@ -0,0 +1,28 @@
+package com.spitzer.network.di
+
+import android.content.Context
+import com.spitzer.network.fake.FakeAssetManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun providesFakeAssetManager(
+        @ApplicationContext context: Context,
+    ): FakeAssetManager = FakeAssetManager(context.assets::open)
+}
