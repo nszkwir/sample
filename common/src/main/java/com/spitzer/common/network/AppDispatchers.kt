0b2d93e27f7534958429d254e71b1a02407b529a
Nicolas Szkwir <nicoszkwir@gmail.com>
2024-02-21 14:35:38 +0100

    Added DI definitions for Dispatchers and CoroutineScopes

---
+++ common/src/main/java/com/spitzer/common/network/AppDispatchers.kt
@@ -0,0 +1,12 @@
+package com.spitzer.common.network
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Dispatcher(val appDispatcher: AppDispatchers)
+
+enum class AppDispatchers {
+    Default,
+    IO,
+}
